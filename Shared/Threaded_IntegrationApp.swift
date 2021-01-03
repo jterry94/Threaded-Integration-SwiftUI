7a2eb4dfe7e0f3c2dca209afcfe50a4a2820920b
Jeff Terry <terryj@iit.edu>
2021-01-03 14:32:54 -0600

    Initial Commit

---
+++ Shared/Threaded_IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threaded_IntegrationApp.swift
+//  Shared
+//
+//  Created by Jeff Terry on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Threaded_IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
